b738a412b15f42901fc7d4e9786881b3e283b488
Roger Bongers <r.l.bongers@gmail.com>
2023-01-30 10:18:42 -0500

    Add total stats

--- src/tools/lsdisj/stats.py
+++ src/tools/lsdisj/stats.py
@@ -30,11 +30,21 @@ files = args.files
 
 # Read files
 
+total_fns = 0
+total_refs = 0
+total_lines = 0
+total_unknown_fns = 0
+total_unknown_refs = 0
+total_unknown_lines = 0
+
 fn_calls = {}
 fn_lines = {}
 fn_end = {}
 fn_refs = {}
 fn_lines = {}
+
+def is_unknown_fn(call_suffix):
+    return bool(re.match('^[0-9a-f]{2}_[0-9a-f]{4}$', call_suffix))
 
 for filename in files:
     current_fn = None
@@ -49,12 +59,14 @@ for filename in files:
                 # Handle call label
                 fn = label_match[0][:-1]
                 current_fn = fn
-                is_unknown = bool(re.match('^[0-9a-f]{2}_[0-9a-f]{4}$', label_match[2]))
+                is_unknown = is_unknown_fn(label_match[2])
+                total_fns += 1
                 if is_unknown:
                     calls = {}
                     fn_calls[fn] = calls
                     fn_lines[fn] = 0
                     fn_end[fn] = False
+                    total_unknown_fns += 1
             elif label_match[1] == 'data':
                 # Handle data label
                 current_fn = None
@@ -67,8 +79,10 @@ for filename in files:
             continue
 
         # Update lines
+        total_lines += 1
         if is_unknown:
             fn_lines[current_fn] += 1
+            total_unknown_lines += 1
 
         # Handle return function
         if re.match('^ *ret( *;|$)', line):
@@ -81,12 +95,17 @@ for filename in files:
             continue
 
         # Parse all unknown call refs
-        for ref in re.findall('call_[a-f0-9]{2}_[a-f0-9]{4}', line):
-            # Record ref
-            fn_refs[ref] = fn_refs.get(ref, 0) + 1
-            # Record call
-            if is_unknown:
-                calls[ref] = True
+        for ref in re.findall('call_([0-9a-f_]*)', line):
+            total_refs += 1
+            if is_unknown_fn(ref):
+                # Repair ref
+                ref = f'call_{ref}'
+                # Record ref
+                fn_refs[ref] = fn_refs.get(ref, 0) + 1
+                total_unknown_refs += 1
+                # Record call
+                if is_unknown:
+                    calls[ref] = True
 
 # Sort statistics
 
@@ -100,3 +119,10 @@ stats = [
 
 for end, calls, refs, lines, fn in sorted(stats):
     print(f'{fn}:\tends: {"yes" if end else "no"}\tunknown calls: {calls}\treferences: {-refs}\tlines: {lines}')
+
+print(f'total functions: {total_fns}')
+print(f'total references: {total_refs}')
+print(f'total lines: {total_lines}')
+print(f'total unknown functions: {total_unknown_fns}')
+print(f'total unknown references: {total_unknown_refs}')
+print(f'total unknown lines: {total_unknown_lines}')
